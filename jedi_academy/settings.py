8d44f5e3f59d9ef96337a461be06000a50ae4ea5
TimurSungatullin <31702763+TimurSungatullin@users.noreply.github.com>
2019-04-19 04:33:45 +0300

    Update settings.py

--- jedi_academy/settings.py
+++ jedi_academy/settings.py
@@ -121,6 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
