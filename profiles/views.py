9f84deb31ef29a0e6cc2ca309384bd8f5a721fde
TimurSungatullin <31702763+TimurSungatullin@users.noreply.github.com>
2019-04-26 15:06:48 +0300

    Add files via upload

--- profiles/views.py
+++ profiles/views.py
@@ -1,11 +1,8 @@
-from smtplib import SMTPAuthenticationError
-
-from django.core import mail
 from django.core.mail import EmailMessage
-from django.db.models import Count, F
+from django.db.models import Count, F, Q
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
-from django.urls import reverse, reverse_lazy
+from django.urls import reverse
 
 from profiles.forms import LoginForm
 from profiles.models import Padawan, Jedi
@@ -14,31 +11,39 @@ from tests.models import Test
 
 def list_jedi(request):
     if request.POST:
-        jedis = Jedi.objects.annotate(count_padawan=Count("padawan_jedi")).filter(count_padawan__gt=1)
+        jedis = Jedi.objects.annotate(count_padawan=Count("padawan_jedi"))\
+            .filter(count_padawan__gt=1)
     else:
         jedis = Jedi.objects.select_related().all()
     return render(request, "profiles/list_jedi.html", {"jedis": jedis})
 
 
 def jedi(request, id):
-    count_padawans = Jedi.objects.count()
+    count_padawans = Jedi.objects.count_padawan(id)
     if count_padawans >= 3:
         return HttpResponse("Вам уже хватит падаванов")
     padawans = Padawan.objects. \
-        filter(planet=jedi.order.planet.pk, jedi=None). \
+        filter(planet__order_planet__jedi_order__pk=id, jedi=None). \
         exclude(result_test=None). \
         order_by("-result_test")
     if request.POST:
-        mails_padawans = padawans.only("email").filter(pk__in=request.POST.getlist('pk'))
-        if count_padawans + mails_padawans.count() >= 3:
+        mails_padawans = padawans.values_list("email", flat=True)\
+            .filter(pk__in=request.POST.getlist('pk'))
+        count_new_padawans = mails_padawans.count()
+        if count_padawans + count_new_padawans > 3:
             return HttpResponse("Вы выбрали слишком много учеников")
-        mails = [mail_padawan.email for mail_padawan in mails_padawans]
-        email = EmailMessage('Jedi academy', 'Вы зачислены в падаваны', to=mails)
+        email = EmailMessage('Jedi academy', 'Вы зачислены в падаваны',
+                             to=mails_padawans)
         email.send()
         for pad in padawans:
-            pad.jedi = id
+            pad.jedi_id = id
             pad.save()
-    return render(request, "profiles/profile_jedi.html", {"padawans": padawans})
+        return redirect(reverse("jedi", args=(id, )))
+    return render(request,
+                  "profiles/profile_jedi.html",
+                  {
+                      "padawans": padawans
+                  })
 
 
 def padawan(request):
@@ -48,7 +53,8 @@ def padawan(request):
         if new_padawan.is_valid():
             new_padawan = new_padawan.save()
             request.session["padawan"] = new_padawan.id
-            test = Test.objects.only("id").filter(order__planet__id=new_padawan.planet.id).first()
+            test = Test.objects.only("id")\
+                .filter(order__planet__id=new_padawan.planet.id).first()
             if test is None:
                 return HttpResponse("Для вашей планеты ещё нет теста")
             return redirect(reverse("test", args=[test.id]))
